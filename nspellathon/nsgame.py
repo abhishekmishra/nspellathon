4ef747de10e0835b4651cf6506c25cf49bc3d5de
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-08-15 16:49:07 +0530

    calculate letters on init

--- nspellathon/nsgame.py
+++ nspellathon/nsgame.py
@@ -16,6 +16,11 @@ class App:
         self.size = self.width, self.height = width, height
         self.spellathon = create_spellathon()
 
+        self.upper_letters = self.spellathon.letters.upper()
+        self.central_letter = self.upper_letters[self.spellathon.central_index]
+        self.upper_letters = self.upper_letters[:self.spellathon.central_index] + \
+            self.upper_letters[self.spellathon.central_index+1:]
+
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(
@@ -34,12 +39,11 @@ class App:
         pass
 
     def on_render(self):
-        self.draw_polygon((0.9 * self.height)/2, 6, (255, 204, 255), self.width/2, self.height/2)
-        self.draw_polygon((0.4 * self.height)/2, 6, (255, 204, 204), self.width/2, self.height/2)
+        self.draw_polygon((0.9 * self.height)/2, 6,
+                          (255, 204, 255), self.width/2, self.height/2)
+        self.draw_polygon((0.4 * self.height)/2, 6,
+                          (255, 204, 204), self.width/2, self.height/2)
 
-        upper_letters = self.spellathon.letters.upper()
-        central_letter = upper_letters[self.spellathon.central_index]
-        upper_letters = upper_letters[:self.spellathon.central_index] + upper_letters[self.spellathon.central_index+1:]
         x = self.width/2
         y = self.height/2
         r = (0.65 * self.height)/2
@@ -48,8 +52,9 @@ class App:
             j = i + 0.5
             px = x + (r * math.cos(2 * math.pi * j / n))
             py = y + (r * math.sin(2 * math.pi * j / n))
-            self.draw_alpha(upper_letters[i].upper(), px, py)
-        self.draw_alpha(self.spellathon.get_central_char().upper(), self.width/2, self.height/2)
+            self.draw_alpha(self.upper_letters[i].upper(), px, py)
+        self.draw_alpha(self.spellathon.get_central_char(
+        ).upper(), self.width/2, self.height/2)
         milliseconds = self.clock.tick(self.fps)
         self.playtime += milliseconds / 1000.0
         self.draw_text("FPS: {:6.3}{}PLAYTIME: {:6.3} SECONDS".format(
@@ -78,7 +83,7 @@ class App:
         surface = self.font.render(text, True, (0, 255, 0))
         # // makes integer division in python3
         self._display_surf.blit(surface, ((self.width - fw) //
-                                   2, (self.height - fh)))
+                                          2, (self.height - fh)))
 
     def draw_alpha(self, text, locx, locy):
         fw, fh = self.font.size(text)  # fw: font width,  fh: font height
@@ -90,7 +95,7 @@ class App:
         # https://stackoverflow.com/questions/3436453/calculate-coordinates-of-a-regular-polygons-vertices
         # for (i = 0; i < n; i++) {
         #  printf("%f %f\n",r * Math.cos(2 * Math.PI * i / n), r * Math.sin(2 * Math.PI * i / n));
-        #}
+        # }
         pts = []
         for i in range(n):
             px = x + (r * math.cos(2 * math.pi * i / n))
@@ -98,6 +103,7 @@ class App:
             #print("{x}={y}".format(x=x, y=y))
             pts.append((px, py))
         pygame.draw.polygon(self._display_surf, c, pts)
+
 
 if __name__ == "__main__":
     theApp = App()
